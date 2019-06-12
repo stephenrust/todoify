83e39c5b65c3b7f9236dc5538fc54c0a77d2a9c9
Stephen Rust <rust.stephen@gmail.com>
2019-06-11 23:33:05 -0500

    Updated language and logic on TodoStats

--- src/components/TodoStats/TodoStats.js
+++ src/components/TodoStats/TodoStats.js
@@ -3,36 +3,41 @@ import React from "react";
 import "./style.css";
 
 const TodoStats = props => {
-  const totalTodos = props.todos.length;
-  let multipleTodos = false;
-
   const openTodos = props.todos.filter(todo => todo.isComplete !== true).length;
 
   const completedTodos = props.todos.filter(todo => todo.isComplete === true)
     .length;
 
-  if (totalTodos > 1) {
-    multipleTodos = true;
-  }
+  const incompleteTodos = props.todos.filter(todo => todo.isComplete !== true)
+    .length;
 
   let openTodoLabel;
-  if (completedTodos > 1) {
-    openTodoLabel = "Todo Items";
+  if (incompleteTodos === 0) {
+    openTodoLabel = "Todos";
+  } else if (incompleteTodos === 1) {
+    openTodoLabel = "Todo";
   } else {
-    openTodoLabel = "Todo Item";
+    openTodoLabel = "Todos";
+  }
+
+  let completeTodoLabel;
+  if (completedTodos === 0) {
+    completeTodoLabel = "Todos";
+  } else if (completedTodos === 1) {
+    completeTodoLabel = "Todo";
+  } else {
+    completeTodoLabel = "Todos";
   }
 
   return (
     <div className="row">
       <div className="todoStatContainer eight wide column">
         <h3 className="todoStat"> {openTodos} </h3>
-        <p className="todoStatLabel">
-          Open {multipleTodos ? "Todo Items" : "Todo Item"}
-        </p>
+        <p className="todoStatLabel">Incomplete {openTodoLabel}</p>
       </div>
       <div className="todoStatContainer eight wide column">
         <h3 className="todoStat">{completedTodos < 1 ? 0 : completedTodos}</h3>
-        <p className="todoStatLabel">Completed {openTodoLabel}</p>
+        <p className="todoStatLabel">Complete {completeTodoLabel}</p>
       </div>
     </div>
   );
