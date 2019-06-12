d09652d0571a5d0b697e361ef0b862ddb017a65e
Stephen Rust <rust.stephen@gmail.com>
2019-06-12 01:26:28 -0500

    Removed old, commented out code from TodoForm

--- src/components/TodoForm/TodoForm.js
+++ src/components/TodoForm/TodoForm.js
@@ -42,42 +42,6 @@ class TodoForm extends Component {
   }
 }
 
-// const TodoForm = ({
-//   todoFormValue,
-//   handleTodoInput,
-//   handleTodoSubmit,
-//   todoFormError
-// }) => {
-//   let todoFormErrorStyle = classNames({
-//     "ui segment errorSegment": true,
-//     hasError: todoFormError
-//   });
-
-//   return (
-//     <div className="ui segment todoForm">
-//       <form className="ui form" onSubmit={handleTodoSubmit}>
-//         <div className="field">
-//           <input
-//             type="text"
-//             placeholder="What do you need to get done?"
-//             value={todoFormValue}
-//             onChange={handleTodoInput}
-//           />
-//         </div>
-//         <div className={todoFormErrorStyle}>
-//           <i className="icon exclamation circle" /> {todoFormError}
-//         </div>
-//         <button
-//           className="ui button fluid submitTodoButton"
-//           onClick={handleTodoSubmit}
-//         >
-//           Add Todo
-//         </button>
-//       </form>
-//     </div>
-//   );
-// };
-
 TodoForm.propTypes = {
   todoFormValue: PropTypes.string,
   todoFormError: PropTypes.string,
